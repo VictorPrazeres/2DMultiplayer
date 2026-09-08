ffbc533ed9e71c1e8dfd682b23453bedca1743c1
Victor Prazeres <dev.vprazeres@gmail.com>
2026-09-08 14:57:30 -0300

    feat(multiplayer): implement basic state synchronization and rpc call
    Synchronize essential player properties across network peers and introduce
    the first RPC call for executing remote actions.

--- main.gd
+++ main.gd
@@ -2,4 +2,9 @@ extends Node
 
 
 func _ready() -> void:
-	print("hello world!")
+	peer_ready.rpc_id(1)
+
+
+@rpc("any_peer", "call_local", "reliable")
+func peer_ready():
+	print("peer %s ready" % multiplayer.get_remote_sender_id())
--- ui/main_menu/main_menu.gd
+++ ui/main_menu/main_menu.gd
@@ -2,6 +2,8 @@ extends Control
 
 const PORT: int = 3000
 
+var main_scene: PackedScene = preload("uid://bwtumf0ncui6b")
+
 @onready var host_button: Button = $HContainer/HostButton
 @onready var join_button: Button = $HContainer/JoinButton
 
@@ -9,13 +11,14 @@ const PORT: int = 3000
 func _ready() -> void:
 	host_button.pressed.connect(_on_host_pressed)
 	join_button.pressed.connect(_on_join_pressed)
-	multiplayer.peer_connected.connect(_on_peer_connected)
+	multiplayer.connected_to_server.connect(_on_connected_to_server)
 
 
 func _on_host_pressed() -> void:
 	var server_peer := ENetMultiplayerPeer.new()
 	server_peer.create_server(PORT)
 	multiplayer.multiplayer_peer = server_peer
+	get_tree().change_scene_to_packed(main_scene)
 
 
 func _on_join_pressed() -> void:
@@ -24,6 +27,5 @@ func _on_join_pressed() -> void:
 	multiplayer.multiplayer_peer = client_peer
 
 
-func _on_peer_connected(id: int):
-	print("my peer id: %s" % multiplayer.get_unique_id())
-	print("peer connected %s" % id)
+func _on_connected_to_server():
+	get_tree().change_scene_to_packed(main_scene)

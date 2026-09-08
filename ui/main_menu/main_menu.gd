48a47f72eae27ac64b1ab7e47d2d89e851c9a09e
Victor Prazeres <dev.vprazeres@gmail.com>
2026-09-08 14:21:15 -0300

    feat(multiplayer): implement initial connection and hosting logic
    Add core scripts to handle host creation, server joining, and initial
    player instantiation across network peers.

--- ui/main_menu/main_menu.gd
+++ ui/main_menu/main_menu.gd
@@ -1,17 +1,29 @@
 extends Control
 
+const PORT: int = 3000
+
 @onready var host_button: Button = $HContainer/HostButton
-@onready var play_button: Button = $HContainer/PlayButton
+@onready var join_button: Button = $HContainer/JoinButton
 
 
 func _ready() -> void:
 	host_button.pressed.connect(_on_host_pressed)
-	play_button.pressed.connect(_on_play_pressed)
+	join_button.pressed.connect(_on_join_pressed)
+	multiplayer.peer_connected.connect(_on_peer_connected)
 
 
 func _on_host_pressed() -> void:
-	print("host pressed")
+	var server_peer := ENetMultiplayerPeer.new()
+	server_peer.create_server(PORT)
+	multiplayer.multiplayer_peer = server_peer
 
 
-func _on_play_pressed() -> void:
-	print("play pressed")
+func _on_join_pressed() -> void:
+	var client_peer := ENetMultiplayerPeer.new()
+	client_peer.create_client("127.0.0.1", PORT)
+	multiplayer.multiplayer_peer = client_peer
+
+
+func _on_peer_connected(id: int):
+	print("my peer id: %s" % multiplayer.get_unique_id())
+	print("peer connected %s" % id)

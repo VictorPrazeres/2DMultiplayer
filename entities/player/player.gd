22e08615a09db99d2bc5c2a7da7c3ef847553855
Victor Prazeres <dev.vprazeres@gmail.com>
2026-09-15 17:07:10 -0300

    feat(weapon): add weapon animations for attack and idle states
    Introduce animation tracks and playback logic for weapons to provide visual
    feedback during attacks and state transitions.

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -8,6 +8,7 @@ var bullet_scene: PackedScene = preload("uid://dp5836u66xfiw")
 @onready var fire_rate_timer: Timer = $FireRateTimer
 @onready var health_component: HealthComponent = $HealthComponent
 @onready var visuals: Node2D = $Visuals
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 
 func _ready() -> void:
@@ -21,7 +22,7 @@ func _process(_delta: float) -> void:
 		velocity = player_input_synchronizer_component.movement_vector * 100
 		move_and_slide()
 		if player_input_synchronizer_component.is_attack_pressed:
-			try_create_bullet()
+			try_fire()
 
 
 func update_aim_position():
@@ -32,14 +33,19 @@ func update_aim_position():
 	weapon_root.look_at(aim_position)
 
 
-func try_create_bullet():
+func try_fire():
 	if !fire_rate_timer.is_stopped():
 		return
+	
 	var bullet = bullet_scene.instantiate() as Bullet
 	bullet.global_position = weapon_root.global_position
 	bullet.start(player_input_synchronizer_component.aim_vector)
 	get_parent().add_child(bullet, true)
 	fire_rate_timer.start()
+	
+	if animation_player.is_playing():
+		animation_player.stop()
+	animation_player.play("fire")
 
 
 func _on_died():

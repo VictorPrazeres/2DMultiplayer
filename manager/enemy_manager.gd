91c6251d3523d336f0151d63931142d70094b767
Victor Prazeres <dev.vprazeres@gmail.com>
2026-09-22 13:51:12 -0300

    feat(gameplay): add victory condition logic
    Evaluate match state to determine when win criteria are met, triggering
    victory events and screens across connected network clients.

--- manager/enemy_manager.gd
+++ manager/enemy_manager.gd
@@ -2,11 +2,13 @@ class_name EnemyManager extends Node
 
 signal round_changed(round_number: int)
 signal round_completed
+signal game_completed
 
 const ROUND_BASE_TIME: int = 10
 const ROUND_GROWTH: int = 5
 const BASE_ENEMY_SPAWN_TIME: float = 2
 const ENEMY_SPAWN_TIME_GROWTH: float = -.15
+const MAX_ROUNDS: int = 10
 
 @export var enemy_scene: PackedScene
 @export var enemy_spawn_root: Node
@@ -82,7 +84,16 @@ func check_round_completed():
 	
 	if spawned_enemies == 0:
 		round_completed.emit()
-		begin_round()
+		
+		if round_count == MAX_ROUNDS:
+			complete_game()
+		else:
+			begin_round()
+
+
+func complete_game():
+	await get_tree().create_timer(2).timeout
+	game_completed.emit()
 
 
 func get_random_spawn_position() -> Vector2:
@@ -109,7 +120,6 @@ func _on_round_timer_timeout():
 	if is_multiplayer_authority():
 		spawn_interval_timer.stop()
 		check_round_completed()
-		print("round over")
 
 
 func _on_enemy_died():
